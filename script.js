4663ae21f2139921a42c0f88b6c96e79e374e987
adriankoder5 <adriankoder5@gmail.com>
2024-11-27 11:12:17 +0100

    looks ok

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log('Hello :3');
+
+const inputData = document.querySelector('#inpD');
+const send = document.querySelector('#sendD');
+console.log(inputData,'--',send);
+
+function displayData() {// viser data fra inputtfeltet i console
+    console.log('funct runn');
+    let valueData = document.querySelector('#inpD').value;
+    console.log(valueData)
+}
+
+send.addEventListener('click', displayData);//button for running displaydata funct
+
+inputData.addEventListener('keydown', function (event) { //enter for running displaydata funct
+  if (event.key === 'Enter') {
+    console.log('enterpressed');
+    displayData();
+  }
+});
