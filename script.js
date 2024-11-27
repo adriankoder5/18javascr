2360dae56c86593a234e24b3f0862fb00444463d
adriankoder5 <adriankoder5@gmail.com>
2024-11-27 12:06:00 +0100

    ferdig

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ console.log('Hello :3');
 
 const inputData = document.querySelector('#inpD');
 const send = document.querySelector('#sendD');
+const parent = document.querySelector('#container');
 console.log(inputData,'--',send);
 
-function displayData() {// viser data fra inputtfeltet i console
+function displayData() {
     console.log('funct runn');
     let valueData = document.querySelector('#inpD').value;
-    console.log(valueData)
+    console.log(valueData)// viser data fra inputtfeltet i console
+    let newInput = document.createElement('li');//ny liste item
+    if (valueData === ''){console.log('write something')}
+    else{newInput.textContent = valueData;//lag liste inhold fra input text
+         parent.appendChild(newInput);}
 }
 
 send.addEventListener('click', displayData);//button for running displaydata funct
